a15ab88bcae108807fe5de39b962150cb25f34d4
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-09-24 23:34:26 -0300

    Error handling finished!

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 import { body } from 'express-validator';
 import { currentUserRouter } from './routes/current-user';
@@ -6,16 +7,21 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
 app.use(json());
-
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+app.all('*', () => {
+  throw new NotFoundError();
+});
 app.use(errorHandler);
+
+
 
 app.listen('3000', () => {
   console.log('Listening on Port 3000!!!!!');
