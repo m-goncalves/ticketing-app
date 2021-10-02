b0bba1e14593a5b69be306e34c65927bc9da8406
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-10-01 22:38:59 -0300

    Sign in credentials being correctly checked

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,26 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationsError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there!');
-  
-});
+router.post('/api/users/signin', 
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid!'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password!')
+  ], 
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+      throw new RequestValidationsError(errors.array());
+    } 
+  }
+);
 
 export { router as signinRouter };
