713c9bd92d66b526df8c8819e2c51432609d61d4
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-10-22 23:47:17 -0300

    Updating connection url

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,12 +11,8 @@ const start = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-    console.log('Connected to MongoDb');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB');
   } catch (err) {
     console.error(err);
   }
