78657509aa5440fe37276b66fd56b14265ec44ee
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-10-22 23:37:16 -0300

    Configuring tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,22 +1,23 @@
 import express from 'express';
 import 'express-async-errors';
-import cookieSession from 'cookie-session';
 import { json } from 'body-parser';
-import { body } from 'express-validator';
+import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@m-goncalves/common';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
-app.use(cookieSession({
-  signed: false,
-  secure: process.env.NODE_ENV !== 'test'
-}));
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
 
-
-app.all('*', () => {
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
+
 app.use(errorHandler);
 
 export { app };
