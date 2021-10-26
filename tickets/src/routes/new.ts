3941c49d7656f9b39c74ad4cc225a300be985a65
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-10-25 23:03:55 -0300

    Setting up tickets service tests

---
+++ tickets/src/routes/new.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+import { requireAuth } from "@m-goncalves/common";
+
+const router = express.Router();
+
+router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
+  res.sendStatus(200);
+
+});
+
+export { router as createTicketRouter };
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,10 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
-import { app } from '../app'
+import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -31,17 +32,21 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email, password
-    })
-    .expect(201);
+global.signin = () => {
 
-  const cookie = response.get('Set-Cookie');
+  const payload = {
+    id: 'dsfasfas',
+    email: 'test@test.com'
+  };
 
-  return cookie;
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const session = {jwt: token};
+
+  const sessionJSON = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  return [`express:sess:${base64}`];
+ 
 };
